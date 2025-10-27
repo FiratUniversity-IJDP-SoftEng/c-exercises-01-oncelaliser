49751f686a2f585acdf84b4deec55366ca9b62ad
Ali Şer <aliseroncel@outlook.com>
2025-10-27 12:27:19 +0300

    happy birthday counter
    Added functionality to print a birthday message based on user input age.

--- ex2.c
+++ ex2.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main() {
+
+	char str[] = "Happy Birthday!";
+
+	printf("Enter your age :");
+	int a;
+	scanf("%d", &a);
+
+	for (int i = 0; i < a; i++) {
+		printf("%s \n", str);
+	}
+
+
+
+
+	return 0;
+}
