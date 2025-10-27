8c612c539c8526d88707f5558da178661ec72cd1
Ali Şer <aliseroncel@outlook.com>
2025-10-27 12:08:44 +0300

    Implement secret number guessing game
    Add a guessing game that prompts user for a secret number.

--- ex1.c
+++ ex1.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+int main()
+{
+
+	int array[5] = { 1, 2, 3, 4, 5 };
+	int secretnumb = 3;
+	for (int i = 0; i < 5; i++) {
+		printf("%d", array[i]);
+	}
+	int n;
+	printf("\nGuess the secret number : \n");
+	scanf("%d", &n);
+	if (n == secretnumb) {
+		printf("Correct answer");
+	}
+	else {
+		printf("Wrong answer");
+
+		return 0;
+	}
+
+}
