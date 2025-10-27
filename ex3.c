1fdb7ae6ac7a7e03f77644ff6430fe13f73daa84
Ali Şer <aliseroncel@outlook.com>
2025-10-27 12:43:55 +0300

    the pet helper

--- ex3.c
+++ ex3.c
@@ -1,2 +1,24 @@
+#include <stdio.h>
+#include <string.h>
 
+int main() {
+
+	printf("Which pet do you have cat or dog : \n");
+	char str[5];
+
+	scanf("%s", &str);
+
+	if (strcmp (str, "dog") == 0) {
+		printf("woof");
+	}
+	else if (strcmp(str, "cat") == 0) {
+		printf("meows");
+	}
+	else {
+		printf("Invalid animal");
+	}
+
+
+	return 0;
+}
 
